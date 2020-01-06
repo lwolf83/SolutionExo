0089f92155581e28f1f694136f94923a96ffc05f
lwolf83 <58726454+lwolf83@users.noreply.github.com>
2020-01-06 10:47:30 +0100

    Update char.cs

--- char.cs
+++ char.cs
@@ -4,7 +4,7 @@ class MainClass {
   public static void Main (string[] args) {
 
     for(int i = 97; i <= 122; i++)
-      Console.WriteLine ((char) i);
+      Console.WriteLine ("{0} = {1}",i,(char) i);
 
   }
 }
