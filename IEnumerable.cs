14116b267e379d67c9c2181642fdc4a14067a1bb
lwolf83 <58726454+lwolf83@users.noreply.github.com>
2020-01-27 10:25:16 +0100

    Create IEnumerable.cs

---
+++ IEnumerable.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace POO___2._Les_extensions_IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {;
+            Random randomGenerateur = new Random();
+            Console.WriteLine("Enter number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int nbvaleur = 25;
+            List<int> tabInt = new List<int>();
+            for(int i = 0; i< nbvaleur; i++)
+            {
+                tabInt.Add(randomGenerateur.Next(0,100));
+            }
+
+            double moyenne = tabInt.Where((x) => x < n).Average();
+
+            foreach(int tempInteger in tabInt)
+            {
+                Console.Write("" + tempInteger + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Mean (n < {0}) = {1}", n, moyenne);
+
+
+        }
+    }
+}
